4c6718a366dc57a65301b71f1f27e1540f6747be
konioa <goodsong0715@gmail.com>
2023-04-25 10:08:03 +0900

    各種ルーティング2

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'tops', to: 'tops#index'
   root to: 'articles#index'
+  resources :users, only: [:edit, :update]
   resources :articles
 end
